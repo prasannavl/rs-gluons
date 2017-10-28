b40ecc1c750533623d8663edb48d054563485b52
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-10-29 02:57:04 +0530

    add: hello-rs-nostd

---
+++ README.md
@@ -0,0 +1,6 @@
+# rust-play
+
+Rust Playground
+
+- **hello-rs-nostd**  
+  Smallest possible statically linked hello world on windows without std-lib - 10k
---
+++ hello-rs-nostd/src/main.rs
@@ -0,0 +1,17 @@
+#![feature(lang_items, core_intrinsics)]
+#![feature(start)]
+#![no_std]
+
+extern crate winapi;
+extern crate kernel32;
+
+use core::ptr;
+
+// Entry point for this program.
+#[start]
+fn start(_argc: isize, _argv: *const *const u8) -> isize {
+    let msg = "Hello world!\n";
+    let stdout = unsafe { kernel32::GetStdHandle(winapi::STD_OUTPUT_HANDLE) };
+    unsafe { kernel32::WriteConsoleA(stdout, msg.as_ptr() as *const winapi::VOID, msg.len() as winapi::DWORD, ptr::null_mut(), ptr::null_mut()) };
+    0
+}
