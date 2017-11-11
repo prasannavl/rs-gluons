179dc0c369ebfd1bddd91eb3434eadabfb6b4e3e
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-11-11 21:07:13 +0530

    change: extract compare fn

--- src/search.rs
+++ src/search.rs
@@ -1,4 +1,10 @@
-pub fn binary_search<T: PartialOrd>(val: &T, slice: &[T]) -> Option<usize> {
+use std::cmp::Ordering;
+use std::cmp::Ordering::*;
+
+pub fn binary_search<T, F>(val: &T, slice: &[T], mut compare: F) -> Option<usize>
+where
+    F: FnMut(&T, &T) -> Ordering,
+{
     let len = slice.len();
     let mut start = 0usize;
     let mut end = len;
@@ -6,12 +12,10 @@ pub fn binary_search<T: PartialOrd>(val: &T, slice: &[T]) -> Option<usize> {
     while start < end {
         middle = start + (end - start) / 2;
         let current = &slice[middle];
-        if val > current {
-            start = middle + 1;
-        } else if val < current {
-            end = middle;
-        } else {
-            return Some(middle);
+        match compare(val, current) {
+            Greater => start = middle + 1,
+            Less => end = middle,
+            Equal => return Some(middle),
         }
     }
     return None;
